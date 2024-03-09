c034a7d4f48d56fe94954a1c070b71bf0a909a5b
RaphaelFreitasDev <freitasraphael.ctba@gmail.com>
2024-03-09 10:23:21 -0300

    repnsi fix

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="container w-[1200px] m-auto my-8 items-center justify-between flex-col lg:flex">
+    <div className="container w-[1200px] m-auto my-8 items-center justify-between flex-col md:flex">
       <h2 className="text-4xl text-red-700 font-semibold">Enfermagem Kit</h2>
-      <div className="flex gap-6">
+      <div className="flex-col lg:flex gap-6">
         <Link
           href={"/pages/gotejamento"}
           className="text-md text-white p-4 hover:text-blue-800 ease-in duration-300"
